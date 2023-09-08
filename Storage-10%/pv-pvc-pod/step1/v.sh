2c7e760ff354296d8b4eb70c4e88cb6607abbc74
RSachinShetty <sachinshetty.ran@gmail.com>
2023-09-08 11:53:00 +0530

    sa Updated 27

--- Storage-10%/pv-pvc-pod/step1/v.sh
+++ Storage-10%/pv-pvc-pod/step1/v.sh
@@ -1,80 +1,88 @@
 #!/bin/bash
 
-# Verify PV
-pv_name="my-pv-cka"
-if kubectl get pv "$pv_name" &> /dev/null; then
-  # Get PV details
-  pv_info=$(kubectl describe pv "$pv_name")
-
-  # Check storage capacity
-  if [[ $pv_info =~ Capacity:[[:space:]]*100Mi ]]; then
-    echo "PV $pv_name has the correct storage capacity."
+# Function to check if a resource exists
+check_resource() {
+  local resource_type="$1"
+  local resource_name="$2"
+  if kubectl get "$resource_type" "$resource_name" &> /dev/null; then
+    echo "$resource_type $resource_name exists."
   else
-    echo "Error: PV $pv_name does not have the correct storage capacity."
+    echo "Error: $resource_type $resource_name does not exist."
     exit 1
   fi
+}
+
+# Verify PV
+check_resource "pv" "my-pv-cka"
+pv_info=$(kubectl describe pv my-pv-cka)
+
+# Check storage capacity
+if [[ $pv_info =~ Capacity:[[:space:]]*100Mi ]]; then
+  echo "PV my-pv-cka has the correct storage capacity."
+else
+  echo "Error: PV my-pv-cka does not have the correct storage capacity."
+  exit 1
+fi
+
+# Check access mode
+pv_name="my-pv-cka"
+if kubectl get pv "$pv_name" &> /dev/null; then
+  # Get PV details in JSON format
+  pv_info_json=$(kubectl get pv "$pv_name" -o json)
+
+  # Extract access mode using Python
+  access_mode=$(kubectl get pv my-pv-cka -o=jsonpath='{.spec.accessModes[*]}')
 
   # Check access mode
-  if [[ $pv_info =~ Access[[:space:]]Modes:[[:space:]]*ReadWriteOnce ]]; then
+  if [[ "$access_mode" == "ReadWriteOnce" ]]; then
     echo "PV $pv_name has the correct access mode."
   else
     echo "Error: PV $pv_name does not have the correct access mode."
     exit 1
   fi
-
-  # Check host path
-  if [[ $pv_info =~ Path:[[:space:]]*/mnt/data ]]; then
-    echo "PV $pv_name has the correct host path."
-  else
-    echo "Error: PV $pv_name does not have the correct host path."
-    exit 1
-  fi
-
 else
   echo "Error: PV $pv_name does not exist."
   exit 1
 fi
 
-# Verify PVC
-pvc_name="my-pvc-cka"
-if kubectl get pvc "$pvc_name" &> /dev/null; then
-  # Get PVC details
-  pvc_info=$(kubectl describe pvc "$pvc_name")
-
-  # Check access mode
-  if [[ $pvc_info =~ Access[[:space:]]Modes:[[:space:]]*ReadWriteOnce ]]; then
-    echo "PVC $pvc_name has the correct access mode."
-  else
-    echo "Error: PVC $pvc_name does not have the correct access mode."
-    exit 1
-  fi
-
-  # Check storage request size (adjust the value as needed)
-  if [[ $pvc_info =~ Storage:[[:space:]]*100Mi ]]; then
-    echo "PVC $pvc_name has the correct storage request size."
-  else
-    echo "Error: PVC $pvc_name does not have the correct storage request size."
-    exit 1
-  fi
-
+# Check host path
+if [[ $pv_info =~ Path:[[:space:]]*/mnt/data ]]; then
+  echo "PV my-pv-cka has the correct host path."
 else
-  echo "Error: PVC $pvc_name does not exist."
+  echo "Error: PV my-pv-cka does not have the correct host path."
+  exit 1
+fi
+
+# Verify PVC
+check_resource "pvc" "my-pvc-cka"
+pvc_info=$(kubectl describe pvc my-pvc-cka)
+
+access_mode=$(kubectl get pvc my-pvc-cka -o=jsonpath='{.spec.accessModes[*]}')
+# Check access mode
+if [[ "$access_mode" == "ReadWriteOnce" ]]; then
+  echo "PVC my-pvc-cka has the correct access mode."
+else
+  echo "Error: PVC my-pvc-cka does not have the correct access mode."
+  exit 1
+fi
+
+# Check storage request size
+storage_value=$(kubectl get pvc -o=jsonpath='{range .items[?(@.spec.resources.requests.storage=="100Mi")]}{@.spec.resources.requests.storage}{end}')
+if [[ $storage_value == "100Mi" ]]; then
+  echo "PVC my-pvc-cka has the correct storage request size."
+else
+  echo "Error: PVC my-pvc-cka does not have the correct storage request size."
   exit 1
 fi
 
 # Verify Pod
-pod_name="my-pod-cka"
-if kubectl get pod "$pod_name" &> /dev/null; then
-  sleep 25  # Wait for 25 seconds to allow the pod to become Running
-  pod_status=$(kubectl get pod "$pod_name" -o jsonpath='{.status.phase}')
-  if [ "$pod_status" == "Running" ]; then
-    echo "Pod $pod_name is in a Running state."
-    exit 0
-  else
-    echo "Error: Pod $pod_name is not in a Running state. Current state: $pod_status"
-    exit 1
-  fi
+check_resource "pod" "my-pod-cka"
+sleep 25  # Wait for 25 seconds to allow the pod to become Running
+pod_status=$(kubectl get pod my-pod-cka -o jsonpath='{.status.phase}')
+if [ "$pod_status" == "Running" ]; then
+  echo "Pod my-pod-cka is in a Running state."
+  exit 0
 else
-  echo "Error: Pod $pod_name does not exist."
+  echo "Error: Pod my-pod-cka is not in a Running state. Current state: $pod_status"
   exit 1
 fi
