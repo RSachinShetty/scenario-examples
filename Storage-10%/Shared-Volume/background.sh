60538378069a717c2d033519e7be7d84f199eff5
RSachinShetty <sachinshetty.ran@gmail.com>
2023-09-08 20:53:11 +0530

    sa Updated 61

--- Storage-10%/Shared-Volume/background.sh
+++ Storage-10%/Shared-Volume/background.sh
@@ -2,18 +2,6 @@
 
 kubectl apply -f - <<EOF
 apiVersion: v1
-kind: PersistentVolume
-metadata:
-  name: my-pv-cka
-spec:
-  capacity:
-    storage: 100Mi
-  accessModes:
-    - ReadWriteOnce
-  hostPath:
-    path: /mnt/data
----
-apiVersion: v1
 kind: PersistentVolumeClaim
 metadata:
   name: my-pvc-cka
