3db4384d130ebc9db5405652b036258d51683e07
RSachinShetty <sachinshetty.ran@gmail.com>
2023-09-10 13:40:09 +0530

    sa Updated 102

--- Storage-10%/nslookup/step1/v.sh
+++ Storage-10%/nslookup/step1/v.sh
@@ -16,30 +16,15 @@ else
     exit 1
 fi
 
-# Perform nslookup for nginx-service-cka and store the output in nginx-service-test.txt
-kubectl run test-nslookup --image=busybox --rm -it --restart=Never -- nslookup nginx-service-cka.default.svc.cluster.local > nginx-service-test.txt
-
-# Extract the relevant lines from nginx-service-test.txt and dns-service.txt
-grep -E 'Server:|Address:|Name:' nginx-service-test.txt > nginx-service-test-extracted.txt
-grep -E 'Server:|Address:|Name:' nginx-service.txt > nginx-service-extracted.txt
-
-# Compare the two extracted files
-if diff -q nginx-service-test-extracted.txt nginx-service-extracted.txt &> /dev/null; then
-    echo "Validation PASSED: DNS resolution matches expected output."
-else
-    echo "Validation FAILED: DNS resolution does not match expected output."
-    exit 1
-fi
-
-# Perform nslookup for nginx-service-cka and store the output in nginx-service-test.txt
-kubectl run test-nslookup-service --image=busybox --rm -it --restart=Never -- nslookup nginx-service-cka.default.svc.cluster.local > nginx-service-test.txt
+# Perform nslookup for dns-service-cka and store the output in dns-service-test.txt
+kubectl run test-nslookup-service --image=busybox --rm -it --restart=Never -- nslookup dns-service-cka.default.svc.cluster.local > dns-service-test.txt
 
 # Extract relevant lines from the test outputs
-grep -E 'Server:|Address:|Name:' nginx-service-test.txt > nginx-service-test-extracted.txt
+grep -E 'Server:|Address:|Name:' dns-service-test.txt > dns-service-test-extracted.txt
 grep -E 'Server:|Address:|Name:' nginx-service.txt > nginx-service-extracted.txt
 
 # Compare the extracted files
-if diff -q nginx-service-test-extracted.txt nginx-service-extracted.txt &> /dev/null; then
+if diff -q dns-service-test-extracted.txt nginx-service-extracted.txt &> /dev/null; then
     echo "Validation PASSED: DNS resolution matches expected output for both pod and service."
 else
     echo "Validation FAILED: DNS resolution does not match expected output for either pod or service."
@@ -47,4 +32,4 @@ else
 fi
 
 # Clean up temporary files
-rm -f nginx-service-test.txt nginx-service-test-extracted.txt
+rm -f dns-service-test.txt dns-service-test-extracted.txt
