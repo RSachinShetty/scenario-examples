b14b7298e2d2e8e0569ad6a68752c028906a8453
RSachinShetty <sachinshetty.ran@gmail.com>
2023-09-10 23:06:08 +0530

    sa Updated 119

--- Storage-10%/coredns-1/step1/v.sh
+++ Storage-10%/coredns-1/step1/v.sh
@@ -2,19 +2,19 @@
 
 flag="false"
 # Check if the ReplicaSet exists
-if kubectl get rs dns-rs-cka -n dns-ns &> /dev/null; then
-    echo "Validation PASSED: ReplicaSet dns-rs-cka exists."
+if kubectl get deployment dns-deploy-cka -n dns-ns &> /dev/null; then
+    echo "Validation PASSED: ReplicaSet dns-deploy-cka exists."
 else
-    echo "Validation FAILED: ReplicaSet dns-rs-cka does not exist."
+    echo "Validation FAILED: ReplicaSet dns-deploy-cka does not exist."
     flag="true"
 fi  
 
 # Check if there are exactly 2 replicas
-replica_count=$(kubectl get rs dns-rs-cka -n dns-ns -o=jsonpath='{.status.replicas}')
+replica_count=$(kubectl get deployment dns-deploy-cka -n dns-ns -o=jsonpath='{.status.replicas}')
 if [ "$replica_count" -eq 2 ]; then
-    echo "Validation PASSED: ReplicaSet dns-rs-cka has 2 replicas."
+    echo "Validation PASSED: ReplicaSet dns-deploy-cka has 2 replicas."
 else
-    echo "Validation FAILED: ReplicaSet dns-rs-cka does not have 2 replicas."
+    echo "Validation FAILED: ReplicaSet dns-deploy-cka does not have 2 replicas."
     flag="true"
 fi
 
@@ -24,27 +24,27 @@ POD_NAME=$(kubectl get pods -n dns-ns -o jsonpath='{.items[0].metadata.name}')
 container_name=$(kubectl get pod -n dns-ns $POD_NAME -o=jsonpath='{.spec.containers[0].name}')
 # Check if the container name matches "dns-container"
 if [ "$container_name" = "dns-container" ]; then
-    echo "Validation PASSED: Container name in dns-rs-cka is dns-container."
+    echo "Validation PASSED: Container name in dns-deploy-cka is dns-container."
 else
-    echo "Validation FAILED: Container name in dns-rs-cka is not dns-container."
+    echo "Validation FAILED: Container name in dns-deploy-cka is not dns-container."
     flag="true"
 fi
 
 # Check if the image used is registry.k8s.io/e2e-test-images/jessie-dnsutils:1.3
 image_name=$(kubectl get pod -n dns-ns -o=jsonpath='{.items[0].spec.containers[0].image}')
 if [ "$image_name" = "registry.k8s.io/e2e-test-images/jessie-dnsutils:1.3" ]; then
-    echo "Validation PASSED: Container image in dns-rs-cka is correct."
+    echo "Validation PASSED: Container image in dns-deploy-cka is correct."
 else
-    echo "Validation FAILED: Container image in dns-rs-cka is incorrect."
+    echo "Validation FAILED: Container image in dns-deploy-cka is incorrect."
     flag="true"
 fi
 
 # Check if the command is set to 'sleep 3600'
 command=$(kubectl get pod -n dns-ns -o=jsonpath='{.items[0].spec.containers[0].command[0]}')
 if [ "$command" = "sleep" ]; then
-    echo "Validation PASSED: Command in dns-rs-cka is set to 'sleep 3600'."
+    echo "Validation PASSED: Command in dns-deploy-cka is set to 'sleep 3600'."
 else
-    echo "Validation FAILED: Command in dns-rs-cka is not set to 'sleep 3600'."
+    echo "Validation FAILED: Command in dns-deploy-cka is not set to 'sleep 3600'."
     flag="true"
 fi
 
