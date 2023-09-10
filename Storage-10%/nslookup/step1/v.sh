2a6dceeda0f8d906a5fa2c2739bbdfea142021ca
RSachinShetty <sachinshetty.ran@gmail.com>
2023-09-10 13:45:48 +0530

    sa Updated 103

--- Storage-10%/nslookup/step1/v.sh
+++ Storage-10%/nslookup/step1/v.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-# Check if dns-pod-cka exists
-if kubectl get pod dns-pod-cka &> /dev/null; then
-    echo "Validation PASSED: dns-pod-cka exists."
+# Check if nginx-pod-cka exists
+if kubectl get pod nginx-pod-cka &> /dev/null; then
+    echo "Validation PASSED: nginx-pod-cka exists."
 else
-    echo "Validation FAILED: dns-pod-cka does not exist."
+    echo "Validation FAILED: nginx-pod-cka does not exist."
     exit 1
 fi
 
-# Check if dns-service-cka service of type ClusterIP exists
-if kubectl get svc dns-service-cka -o jsonpath='{.spec.type}' | grep -q 'ClusterIP'; then
-    echo "Validation PASSED: dns-service-cka of type ClusterIP exists."
+# Check if nginx-service-cka service of type ClusterIP exists
+if kubectl get svc nginx-service-cka -o jsonpath='{.spec.type}' | grep -q 'ClusterIP'; then
+    echo "Validation PASSED: nginx-service-cka of type ClusterIP exists."
 else
-    echo "Validation FAILED: dns-service-cka of type ClusterIP does not exist."
+    echo "Validation FAILED: nginx-service-cka of type ClusterIP does not exist."
     exit 1
 fi
 
-# Perform nslookup for dns-service-cka and store the output in dns-service-test.txt
-kubectl run test-nslookup-service --image=busybox --rm -it --restart=Never -- nslookup dns-service-cka.default.svc.cluster.local > dns-service-test.txt
+# Perform nslookup for nginx-service-cka and store the output in nginx-service-test.txt
+kubectl run test-nslookup-service --image=busybox --rm -it --restart=Never -- nslookup nginx-service-cka.default.svc.cluster.local > nginx-service-test.txt
 
 # Extract relevant lines from the test outputs
-grep -E 'Server:|Address:|Name:' dns-service-test.txt > dns-service-test-extracted.txt
+grep -E 'Server:|Address:|Name:' nginx-service-test.txt > nginx-service-test-extracted.txt
 grep -E 'Server:|Address:|Name:' nginx-service.txt > nginx-service-extracted.txt
 
 # Compare the extracted files
-if diff -q dns-service-test-extracted.txt nginx-service-extracted.txt &> /dev/null; then
+if diff -q nginx-service-test-extracted.txt nginx-service-extracted.txt &> /dev/null; then
     echo "Validation PASSED: DNS resolution matches expected output for both pod and service."
 else
     echo "Validation FAILED: DNS resolution does not match expected output for either pod or service."
@@ -32,4 +32,4 @@ else
 fi
 
 # Clean up temporary files
-rm -f dns-service-test.txt dns-service-test-extracted.txt
+rm -f nginx-service-test.txt nginx-service-test-extracted.txt
