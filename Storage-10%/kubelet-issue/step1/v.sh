faf753f46b3d260bc0bcf199b6bc7a405dd341f2
RSachinShetty <sachinshetty.ran@gmail.com>
2023-09-16 16:47:49 +0530

    sa Updated 144

--- Storage-10%/kubelet-issue/step1/v.sh
+++ Storage-10%/kubelet-issue/step1/v.sh
@@ -2,11 +2,20 @@
 
 # Specify the file path and the content to search for (case-insensitive)
 FILE_PATH="/var/lib/kubelet/config.yaml"
+FILE_PATH1="/etc/kubernetes/kubelet.conf"
+
 EXPECTED_CONTENT="/etc/kubernetes/pki/ca.crt"
+EXPECTED_CONTENT1="https://172.30.1.2:6443"
 
 # Check if the file exists
 if [ ! -f "$FILE_PATH" ]; then
   echo "File '$FILE_PATH' does not exist."
+  exit 1
+fi
+
+# Check if the file exists
+if [ ! -f "$FILE_PATH1" ]; then
+  echo "File '$FILE_PATH1' does not exist."
   exit 1
 fi
 
@@ -16,6 +25,15 @@ if grep -q -i "$EXPECTED_CONTENT" "$FILE_PATH"; then
   exit 0
 else
   echo "File '$FILE_PATH' does not contain the expected content '$EXPECTED_CONTENT' (case-insensitive)."
+  exit 1
+fi
+
+# Perform a case-insensitive search for the content
+if grep -q -i "$EXPECTED_CONTENT1" "$FILE_PATH1"; then
+  echo "File '$FILE_PATH1' contains the expected content '$EXPECTED_CONTENT1' (case-insensitive)."
+  exit 0
+else
+  echo "File '$FILE_PATH1' does not contain the expected content '$EXPECTED_CONTENT1' (case-insensitive)."
   exit 1
 fi
 
