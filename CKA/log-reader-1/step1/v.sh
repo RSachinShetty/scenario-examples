b74a704643f533b016d3cacba54ace32daa412fe
RSachinShetty <sachinshetty.ran@gmail.com>
2023-09-17 20:22:39 +0530

    sa Updated 156

--- CKA/log-reader-1/step1/v.sh
+++ CKA/log-reader-1/step1/v.sh
@@ -11,12 +11,11 @@ INFO: Mon Oct 1 10:30:23 UTC 2023 Logged In
 ERROR: Mon Oct 1 10:30:23 UTC 2023  Log in Error!"
 
 # Read the content of allpod.txt
-ACTUAL_CONTENT=$(cat allpod.txt)
+ACTUAL_CONTENT=$(cat podlogs.txt)
 
 # Compare the actual content with the expected content
 if [ "$EXPECTED_CONTENT" = "$ACTUAL_CONTENT" ]; then
   echo "Validation passed: The content in allpod.txt matches the expected pattern."
-  exit 0
 else
   echo "Validation failed: The content in allpod.txt does not match the expected pattern."
   exit 1
--- CKA/log-reader-2/step1/q.md
+++ CKA/log-reader-2/step1/q.md
@@ -1,5 +1,5 @@
 
-> <strong>Useful Resources</strong>: [Persistent Volumes](https://kubernetes.io/docs/concepts/storage/persistent-volumes/)
+> <strong>Useful Resources</strong>: [Kubectl](https://kubernetes.io/docs/reference/kubectl/cheatsheet/)
 
 For this question, please set this context (In exam, diff cluster name)
 
