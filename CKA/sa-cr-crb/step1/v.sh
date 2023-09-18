a4a7e829cc021d9a52406e0c558051416246a49a
RSachinShetty <sachinshetty.ran@gmail.com>
2023-09-18 08:04:11 +0530

    sa Updated 168

--- CKA/sa-cr-crb/step1/v.sh
+++ CKA/sa-cr-crb/step1/v.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Validate Service Account
 sa_exists=$(kubectl get serviceaccount group1-sa -n default -o custom-columns=:.metadata.name --no-headers)
 if [ "$sa_exists" != "group1-sa" ]; then
@@ -26,14 +28,13 @@ done
 
 # Validate ClusterRole resources
 role_resources=$(kubectl get clusterrole group1-role-cka -o jsonpath='{.rules[0].resources[*]}' --no-headers)
-expected_resources=("deployment")
+expected_resources=("deployments")
 if [ "$role_resources" != "${expected_resources[*]}" ]; then
     echo "ClusterRole 'group1-role-cka' has incorrect resources."
     exit 1
 fi
 
 echo "ClusterRole 'group1-role-cka' has the expected rules."
-
 
 
 # Validate ClusterRoleBinding
