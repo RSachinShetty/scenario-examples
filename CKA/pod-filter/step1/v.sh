64aea4365c8aede5e6787772af663ab500803318
RSachinShetty <sachinshetty.ran@gmail.com>
2023-09-18 07:43:35 +0530

    sa Updated 166

--- CKA/pod-filter/step1/v.sh
+++ CKA/pod-filter/step1/v.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-# Define the path to the pod-filter.sh script
+# Define the path to the script file
 script_path="pod-filter.sh"
 
-# Check if the script contains the desired command
-if grep -q "kubectl get pod nginx-pod -o jsonpath='{.metadata.labels.application}'" "$script_path"; then
-  echo "Validation Successful: The command exists in the script."
-  exit 0
+# Define the commands to search for
+commands=("kubectl get pod nginx-pod -o=jsonpath='{.metadata.labels.application}'" "kubectl get pods nginx-pod -o=jsonpath='{.metadata.labels.application}'")
+
+# Flag to indicate if at least one command is found
+found=false
+
+# Read the script file line by line
+while IFS= read -r line; do
+  for cmd in "${commands[@]}"; do
+    if [[ $line == *"$cmd"* ]]; then
+      found=true
+      break
+    fi
+  done
+done < "$script_path"
+
+# Check if at least one command is found
+if [ "$found" = true ]; then
+  echo "Validation Successful: At least one of the commands exists in the script."
 else
-  echo "Validation Failed: The command does not exist in the script."
+  echo "Validation Failed: Neither of the commands exists in the script."
   exit 1
 fi
