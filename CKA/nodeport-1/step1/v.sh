9af1f5f96cf23769004cc77701198d7a7027438d
RSachinShetty <sachinshetty.ran@gmail.com>
2023-09-19 15:00:34 +0530

    sa Updated 188

--- CKA/nodeport-1/step1/v.sh
+++ CKA/nodeport-1/step1/v.sh
@@ -2,56 +2,70 @@
 
 # Check if the deployment exists
 if kubectl get deployment my-web-app-deployment &> /dev/null; then
-    echo "Validation PASSED: Deployment my-web-app-deployment exists."
+  echo "Deployment my-web-app-deployment exists."
 else
-    echo "Validation FAILED: Deployment my-web-app-deployment does not exist."
-    exit 1
+  echo "Deployment my-web-app-deployment does not exist."
+  exit 1
 fi
 
-# Check if there are exactly 2 replicas in the deployment
-replica_count=$(kubectl get deployment my-web-app-deployment -o=jsonpath='{.status.replicas}')
-if [ "$replica_count" -eq 2 ]; then
-    echo "Validation PASSED: Deployment my-web-app-deployment has 2 replicas."
+# Check if the number of replicas is 2
+replicas=$(kubectl get deployment my-web-app-deployment -o=jsonpath='{.spec.replicas}')
+if [ "$replicas" -eq 2 ]; then
+  echo "Deployment my-web-app-deployment has 2 replicas."
 else
-    echo "Validation FAILED: Deployment my-web-app-deployment does not have 2 replicas."
-    exit 1
+  echo "Deployment my-web-app-deployment does not have 2 replicas (Current: $replicas)."
+  exit 1
 fi
 
-# Check if the deployment uses the correct Docker image
-deployment_image=$(kubectl get deployment my-web-app-deployment -o=jsonpath='{.spec.template.spec.containers[0].image}')
-if [ "$deployment_image" == "wordpress" ]; then
-    echo "Validation PASSED: Deployment my-web-app-deployment uses the correct Docker image."
+# Check if the image is wordpress
+image=$(kubectl get deployment my-web-app-deployment -o=jsonpath='{.spec.template.spec.containers[0].image}')
+if [ "$image" = "wordpress" ]; then
+  echo "Deployment my-web-app-deployment uses the wordpress image."
 else
-    echo "Validation FAILED: Deployment my-web-app-deployment does not use the correct Docker image."
-    exit 1
+  echo "Deployment my-web-app-deployment does not use the wordpress image (Current: $image)."
+  exit 1
 fi
 
 # Check if the service exists
-if kubectl get svc my-web-app-service &> /dev/null; then
-    echo "Validation PASSED: Service my-web-app-service exists."
+if kubectl get service my-web-app-service &> /dev/null; then
+  echo "Service my-web-app-service exists."
 else
-    echo "Validation FAILED: Service my-web-app-service does not exist."
-    exit 1
+  echo "Service my-web-app-service does not exist."
+  exit 1
 fi
 
 # Check if the service type is NodePort
-service_type=$(kubectl get svc my-web-app-service -o=jsonpath='{.spec.type}')
-if [ "$service_type" == "NodePort" ]; then
-    echo "Validation PASSED: Service my-web-app-service is of type NodePort."
+service_type=$(kubectl get service my-web-app-service -o=jsonpath='{.spec.type}')
+if [ "$service_type" = "NodePort" ]; then
+  echo "Service my-web-app-service is of type NodePort."
 else
-    echo "Validation FAILED: Service my-web-app-service is not of type NodePort."
-    exit 1
+  echo "Service my-web-app-service is not of type NodePort (Current: $service_type)."
+  exit 1
 fi
 
-# Check if the service's targetPort and nodePort are correctly set to 80
-target_port=$(kubectl get svc my-web-app-service -o=jsonpath='{.spec.ports[0].targetPort}')
-node_port=$(kubectl get svc my-web-app-service -o=jsonpath='{.spec.ports[0].nodePort}')
-
-if [ "$target_port" == "80" ] && [ "$node_port" == "30770" ]; then
-    echo "Validation PASSED: Service my-web-app-service has the correct targetPort and nodePort."
+# Check if the service port is 80
+service_port=$(kubectl get service my-web-app-service -o=jsonpath='{.spec.ports[0].port}')
+if [ "$service_port" -eq 80 ]; then
+  echo "Service my-web-app-service has port 80."
 else
-    echo "Validation FAILED: Service my-web-app-service does not have the correct targetPort and nodePort."
-    exit 1
+  echo "Service my-web-app-service does not have port 80 (Current: $service_port)."
+  exit 1
 fi
 
-echo "Validation PASSED"
+# Check if the service target port is 80
+target_port=$(kubectl get service my-web-app-service -o=jsonpath='{.spec.ports[0].targetPort}')
+if [ "$target_port" -eq 80 ]; then
+  echo "Service my-web-app-service has targetPort 80."
+else
+  echo "Service my-web-app-service does not have targetPort 80 (Current: $target_port)."
+  exit 1
+fi
+
+# Check if the service node port is 30770
+node_port=$(kubectl get service my-web-app-service -o=jsonpath='{.spec.ports[0].nodePort}')
+if [ "$node_port" -eq 30770 ]; then
+  echo "Service my-web-app-service has nodePort 30770."
+else
+  echo "Service my-web-app-service does not have nodePort 30770 (Current: $node_port)."
+  exit 1
+fi
