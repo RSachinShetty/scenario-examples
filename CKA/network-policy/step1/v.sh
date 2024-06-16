3a338eaf23799ca37d70676d52ecb955d6716123
RSachinShetty <sachinhr.ram@gmail.com>
2024-06-16 13:16:50 +0530

    sa Updated 232

--- CKA/network-policy/step1/v.sh
+++ CKA/network-policy/step1/v.sh
@@ -10,7 +10,7 @@ fi
 
 # Check if the NetworkPolicy specifies 'Ingress' and 'Egress' policy types
 policy_types=$(kubectl get networkpolicy my-app-network-policy -o=jsonpath='{.spec.policyTypes[*]}')
-if [ "$policy_types" == "Ingress Egress" ]; then
+if [ "$policy_types" = "Ingress Egress" ] || [ "$policy_types" = "Egress Ingress" ]; then
     echo "Validation PASSED: NetworkPolicy specifies 'Ingress' and 'Egress' policy types."
 else
     echo "Validation FAILED: NetworkPolicy does not specify both 'Ingress' and 'Egress' policy types."
@@ -19,7 +19,7 @@ fi
 
 # Check if the NetworkPolicy specifies the correct podSelector
 pod_selector=$(kubectl get networkpolicy my-app-network-policy -o=jsonpath='{.spec.podSelector.matchLabels.app}')
-if [ "$pod_selector" == "my-app" ]; then
+if [ "$pod_selector" = "my-app" ]; then
     echo "Validation PASSED: NetworkPolicy specifies the correct podSelector."
 else
     echo "Validation FAILED: NetworkPolicy does not specify the correct podSelector."
@@ -28,7 +28,7 @@ fi
 
 # Check if the NetworkPolicy allows incoming traffic from any pod within the same namespace
 ingress_from_any=$(kubectl get networkpolicy my-app-network-policy -o=jsonpath='{.spec.ingress[0].from[0].podSelector}')
-if [ "$ingress_from_any" == "{}" ]; then
+if [ "$ingress_from_any" = "{}" ]; then
     echo "Validation PASSED: NetworkPolicy allows incoming traffic from any pod within the same namespace."
 else
     echo "Validation FAILED: NetworkPolicy does not allow incoming traffic from any pod within the same namespace."
@@ -37,7 +37,7 @@ fi
 
 # Check if the NetworkPolicy allows incoming traffic from pods labeled as "app=trusted" on port 80
 ingress_from_trusted=$(kubectl get networkpolicy my-app-network-policy -o=jsonpath='{.spec.ingress[1].from[0].podSelector.matchLabels.app}')
-if [ "$ingress_from_trusted" == "trusted" ]; then
+if [ "$ingress_from_trusted" = "trusted" ]; then
     echo "Validation PASSED: NetworkPolicy allows incoming traffic from pods labeled as 'app=trusted' on port 80."
 else
     echo "Validation FAILED: NetworkPolicy does not allow incoming traffic from pods labeled as 'app=trusted' on port 80."
@@ -46,7 +46,7 @@ fi
 
 # Check if the NetworkPolicy allows outgoing traffic to any pod within the same namespace
 egress_to_any=$(kubectl get networkpolicy my-app-network-policy -o=jsonpath='{.spec.egress[0].to[0].podSelector}')
-if [ "$egress_to_any" == "{}" ]; then
+if [ "$egress_to_any" = "{}" ]; then
     echo "Validation PASSED: NetworkPolicy allows outgoing traffic to any pod within the same namespace."
 else
     echo "Validation FAILED: NetworkPolicy does not allow outgoing traffic to any pod within the same namespace."
